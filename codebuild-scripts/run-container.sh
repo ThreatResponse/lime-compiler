cc7d4d12563bbd01936c26a341b98dbf4370ea49
Joel Ferrier <joel@ferrier.io>
2017-03-05 15:09:44 -0800

    download encrypted gpg key outside of lime-compiler container

--- codebuild-scripts/run-container.sh
+++ codebuild-scripts/run-container.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARSER_SCRIPT="${BASE_DIR}/set-session-token.py"
-
-#fetch temporary session token and pass to container
-RESPONSE=$(curl 169.254.169.254$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)
-echo $RESPONSE | python $PARSER_SCRIPT
-
 #run container with env vars
 docker run --rm --name lime-compiler-default \
   -v /var/run/docker.sock:/var/run/docker.sock \
@@ -18,7 +11,4 @@ docker run --rm --name lime-compiler-default \
   -e ARCHIVE_ROOT="$ARCHIVE_ROOT" \
   -e SIGNING_ARGS="$SIGNING_ARGS" \
   -e OPTIONAL_ARGS="$OPTIONAL_ARGS" \
-  -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-  -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-  -e AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" \
   joelferrier/lime-compiler:latest
