ad65679b90513586630d44a922b337c84cfd84de
Joel Ferrier <joel@ferrier.io>
2017-07-12 20:56:42 -0700

    initial lime_compiler/configuration tests

---
+++ spec/configuration_spec.rb
@@ -0,0 +1,54 @@
+require 'lime-compiler/configuration'
+
+describe LimeCompiler::Configuration do
+
+  context "creating" do
+    context "given a new configuration" do
+      config = LimeCompiler::Configuration.new
+      it "responds to config sections messages" do
+        LimeCompiler::Configuration::CONFIG_SECTIONS.each do |section|
+          expect(config.respond_to?(section)).to eq(true)
+        end
+      end
+
+      it "has default config sections" do
+        LimeCompiler::Configuration::CONFIG_SECTIONS.each do |section|
+          expect(config.send(section)).not_to eq(nil)
+        end
+      end
+    end
+  end
+
+  describe "#reload!" do
+    context "given a modified configuration" do
+      config = LimeCompiler::Configuration.new
+      default = config.common.verbose
+      config.common.verbose = !default
+
+      context "after calling #reload!" do
+        it "resets modified configuration" do
+          config.reload!
+          expect(config.common.verbose).to eq(default)
+        end
+      end
+    end
+  end
+
+  describe "#set_flag" do
+    context "given name ':verbose' and value 'true'" do
+      config = LimeCompiler::Configuration.new
+      it "stores the flag" do
+        expect(config.common.verbose).to eq(false)
+
+        config.set_flag(:verbose, true)
+        expect(config.common.verbose).to eq(true)
+      end
+
+      it "persists the flag on #reload!" do
+        config.reload!
+        expect(config.common.verbose).to eq(true)
+      end
+    end
+  end
+
+end
